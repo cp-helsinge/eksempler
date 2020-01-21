650fcb02f1ebe4572f304b759ec746ced6c942fc
Martin Kristensen <MKRI@JOHANNESFOG.DK>
2020-01-21 10:24:28 +0100

    test af forskellige muligheder

--- turtle_test.py
+++ turtle_test.py
@@ -1,7 +1,16 @@
 import turtle
+# range = antal gange
+# rigth or left = grader
+# forward or backward = længde på streg + retning
+# kan man tegne flere samtidig? hvordan
 
-for n in range(1,100):
-    turtle.left(45)
-    turtle.forward(n*7)
-
-input()
+for n in range(1,80):
+    turtle.left(80)
+    turtle.forward(n*2)
+    turtle.right(40)
+    turtle.backward(n*2)
+for n in range(1,50):
+    turtle.right(80)
+    turtle.forward(n*1.5)
+    
+    input()
