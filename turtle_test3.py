a62a3bb85dc660bd9384b213582e011692490be8
Martin Kristensen <MKRI@JOHANNESFOG.DK>
2020-01-23 19:32:57 +0100

    flere turtle

--- turtle_test3.py
+++ turtle_test3.py
@@ -6,13 +6,9 @@ import turtle
 # kan man tegne flere samtidig? hvordan
 ###################################################
 
-for n in range(1,80):
-    turtle.left(80)
-    turtle.forward(n*2)
-    turtle.right(40)
-    turtle.backward(n*2)
-for n in range(1,50):
-    turtle.right(80)
-    turtle.forward(n*1.5)
+for n in range(1,100):
+    turtle.left(90)
+    turtle.forward(n*3)
     
-    input()
+    
+input()
