000e3daad861181b594a368706252efc6610f7a4
paragi <simon.riget@gmail.com>
2020-02-11 11:59:16 +0100

    Fibonacci funktion

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,6 @@
 a=1
 b=1
-for n in range(1,20):
+for n in range(1,10):
   print(a, end =" "),
   c=a+b
   a=b
---
+++ fibonacci2.py
@@ -0,0 +1,26 @@
+# Omsæt specialiseret formel til algoritme, for hurtig beregning af Fibonacci tal.
+# (med brug af closed form funktion)
+#
+# F(n) = 1/sqet(5) * ((1+sqrt(5)) /2 ) ** n - 1/sqet(5) * ((1-sqrt(5)) /2 ) ** n
+#
+# Se forklaring here: http://mathonline.wikidot.com/a-closed-form-of-the-fibonacci-sequence
+
+import math
+
+# Tildel værdier til nogle "magiske" konstanter (For hurtigere beregning)
+sqrt5 = math.sqrt(5)
+factor = 1 / sqrt5;
+k1 = (1 + sqrt5) / 2
+k2 = (1 - sqrt5) / 2
+
+def fibonacci(n):
+  f = factor  * k1 ** n - factor * k2 ** n  # Sammensæt formel
+  return round(f) # Afrund, for at fjerne afrundingsfejl
+
+for n in range(1,100):
+  print(fibonacci(n))
+
+
+
+
+
