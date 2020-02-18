9135885fdb46082adb35eaa960f0369e50d7ab01
paragi <simon.riget@gmail.com>
2020-02-18 16:51:11 +0100

    .

--- 3.1-if.py
+++ 3.1-if.py
@@ -1,6 +1,8 @@
-# opfyld en betingelse
+# hvis en betingelse
 navn = input("Hvad hedder du? ")
 if navn == "Jesper":
     print("Hej", navn)
+
+# Ellers
 else:
     print("Jeg har hørt pænere navne ;-)")
