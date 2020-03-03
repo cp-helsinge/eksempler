9e362cbc578908eb4c3944f090c287cd16e7f509
paragi <simon.riget@gmail.com>
2020-03-03 17:21:57 +0100

    .

--- mod1.py
+++ mod1.py
@@ -1,7 +1,6 @@
 # Module 1
 import turtle
 
-<<<<<<< HEAD
 #def square(size = 40):
 
 size=40
@@ -9,9 +8,3 @@ for n in range(1,5):
   turtle.forward(size)
   turtle.left(90)
 
-=======
-def square(size = 40):
-  for n in range(1,5):
-    turtle.forward(size)
-    turtle.left(90)
->>>>>>> bdd68dcfffddfa0bff9cb6a87242f0d73b22356d
