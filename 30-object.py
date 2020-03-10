37b4db300e8e74edf1858695a4c0d478f996c5d5
paragi <simon.riget@gmail.com>
2020-03-10 10:31:44 +0100

    ,

--- 30-object.py
+++ 30-object.py
@@ -1,1 +1,43 @@
+# -*- coding: utf-8 -*-
 # Objekter
+
+# Terminologi: klasser, objekter, der indeholder variabler og funktioner.
+
+class Test1:
+    def __init__(self): # Constructor function
+        self.a =1   # Public
+        self._d=4   # Protected
+        self.__e=5  # Private
+
+# Nedarvning
+class Test2(Test1):
+    def __init__(self):
+        Test1.__init__(self)
+        self.b = self.a+1
+
+# Nedarvning af flere classer
+class Test3(Test2,Test1):
+    def __init__(self):
+        Test1.__init__(self)
+        Test2.__init__(self)
+        self.c = 3
+
+    # Overloading
+    def __add__(self, o ):
+        for k, v in vars( o ).items():
+            self.__dict__[k] += v
+        return self
+
+    def __sub__(self, o ):
+        for k, v in vars( o ).items():
+            self.__dict__[k] -= v
+        return self
+
+o1 = Test3()
+o2 = Test3()
+
+print( vars(o1) )
+
+print( vars(o1 + o2))
+
+
