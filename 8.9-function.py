524049fc73c24453d25df0f1778445f8a0837b14
paragi <simon@paragi.dk>
2020-10-20 17:52:47 +0200

    turlte function

---
+++ 8.9-function.py
@@ -0,0 +1,8 @@
+import turtle
+from function import *
+
+turtle.speed(3)
+
+for n in range(10):
+    square(100)
+    turtle.left(36)
---
+++ function.py
@@ -0,0 +1,6 @@
+import turtle
+
+def square(size):
+    for n in range(4):
+        turtle.forward(size)
+        turtle.left(90)
