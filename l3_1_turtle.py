24d17e2506362bf844f39b0098973633f9935686
paragi <simon.riget@gmail.com>
2021-10-12 17:59:16 +0200

    .

--- l3_1_turtle.py
+++ l3_1_turtle.py
@@ -2,15 +2,11 @@
 import turtle
 
 turtle.forward(100)
-turtle.left(90)
+turtle.right(90)
 turtle.forward(100)
 turtle.right(90)
-turtle.forward(200)
-turtle.left(45)
 turtle.forward(100)
-turtle.penup()
-turtle.goto(200,200)
-turtle.pendown()
+turtle.right(90)
 turtle.forward(100)
 
 input()
